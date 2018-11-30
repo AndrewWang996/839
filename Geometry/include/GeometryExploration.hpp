7834f943d55708c86266777d1327aec256b6e591
Andy Wang <andrewwang996@gmail.com>
2018-11-29 19:02:58 -0500

    [3.1] 2D convex hull

--- Geometry/include/GeometryExploration.hpp
+++ Geometry/include/GeometryExploration.hpp
@@ -12,6 +12,31 @@ namespace geometry {
     template <typename T>
     using VectorX = Eigen::Matrix<T, Eigen::Dynamic, 1>;
 
+    /*
+    Returns v where
+     - v > 0 if ccw
+     - v < 0 if clockwise
+     - v == 0 if collinear
+    */
+    template <typename T>
+    T CCW(const Vector2<T>& p1, const Vector2<T>& p2, const Vector2<T>& p3) {
+        return (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0]);
+    }
+
+
+    template <typename T>
+    int lowestPointIndex(const std::vector<Vector2<T>>& points) {
+        int i_best = 0;
+        for (int i=0; i<points.size(); i++) {
+            if (points[i][1] < points[i_best][1]) {
+                i_best = i;
+            }
+        }
+        return i_best;
+    }
+
+
+
     // Q1: implement Graham Scan for 2D convex hull
     // The input is a vector of 2D points
     // The output should be the 2D points on the convex hull
@@ -19,7 +44,42 @@ namespace geometry {
     // for comparing with the reference result
     template <typename T>
     std::vector<Vector2<T>> ConvexHull2D(const std::vector<Vector2<T>> &points) {
-        return points;
+        int N = points.size();
+        
+        // make copy of points
+        std::vector<Vector2<T>> ps = points;
+
+        // Determine point with lowest y coordinate
+        int i_best = lowestPointIndex(ps);
+        
+        // Swap with first element
+        std::swap(ps[0], ps[i_best]);
+
+        // Sort points by polar angle
+        std::sort(
+            ps.begin() + 1, 
+            ps.end(), 
+            [pts=ps](Vector2<T> p1, Vector2<T> p2) {
+                return CCW(pts[0], p1, p2) > 0;
+            }
+        );        
+
+        // Instantiate stack
+        std::vector<Vector2<T>> stack { ps[0], ps[1], ps[2] };
+       
+        // Get convex hull 
+        for (int i=3; i<ps.size(); i++) {
+            while (CCW(
+                    stack[stack.size()-2], 
+                    stack[stack.size()-1], 
+                    ps[i]) <= 0) {
+                stack.pop_back();
+            }
+            stack.push_back(ps[i]);
+        }
+
+        // return convex hull (elements in stack)
+        return stack;
     }
 
     // Q2: implement brute-force method for Nd Pareto front
@@ -51,4 +111,7 @@ namespace geometry {
     std::vector<Vector3<T>> ParetoFront3D(const std::vector<Vector3<T>> &points) {
         return points;
     }
+
+
+
 }
